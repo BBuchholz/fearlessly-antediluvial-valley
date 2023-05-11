2ad9b4f1af726565fba5c0bdf2e3af22750435bb
Brent Buchholz <brentbuchholz@gmail.com>
2023-05-11 18:56:56 -0500

    Add nav

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view />
+  <div>
+    <nav>
+      <router-link to="/"> Home </router-link> |
+      <router-link to="/feed"> Feed </router-link> |
+      <router-link to="/register"> Register </router-link> |
+      <router-link to="/sign-in"> Login </router-link> |
+    </nav>
+    <router-view />
+  </div>
 </template>
