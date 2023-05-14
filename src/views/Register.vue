bd09162cf2ccdda7d034cf6ef233527f0da07fa4
Brent Buchholz <brentbuchholz@gmail.com>
2023-05-14 17:44:41 -0500

    Temporarily comment out firebase for troubleshooting

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,23 +7,23 @@
   
   <script setup>
     import { ref } from 'vue'
-    import firebase from 'firebase'
+    // import firebase from 'firebase'
     import { useRouter } from 'vue-router' // import router
     const email = ref('')
     const password = ref('')
     const router = useRouter() // get a reference to our vue router
     const register = () => {
-      firebase
-        .auth() // get the auth api
-        .createUserWithEmailAndPassword(email.value, password.value) // need .value because ref()
-        .then((data) => {
-          console.log('Successfully registered!');
-          router.push('/feed') // redirect to the feed
-        })
-        .catch(error => {
-          console.log(error.code)
-          alert(error.message);
-        });
+      // firebase
+      //   .auth() // get the auth api
+      //   .createUserWithEmailAndPassword(email.value, password.value) // need .value because ref()
+      //   .then((data) => {
+      //     console.log('Successfully registered!');
+      //     router.push('/feed') // redirect to the feed
+      //   })
+      //   .catch(error => {
+      //     console.log(error.code)
+      //     alert(error.message);
+      //   });
     }
   </script>
   
