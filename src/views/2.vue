ec9d7c0a8b89359528492ba43000456b73bd4d6a
Brent Buchholz <brentbuchholz@gmail.com>
2023-05-15 13:51:40 -0500

    Add 2 and 3

--- src/views/2.vue
+++ src/views/2.vue
@@ -1,3 +1,31 @@
 <template>
-    FILL THIS IN
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-e45j"><span style="font-weight:var(--table-header-weight)">Ψ</span></th>
+    <th class="tg-e45j"><span style="font-weight:var(--table-header-weight)">Δ</span></th>
+    <th class="tg-e45j"><span style="font-weight:var(--table-header-weight)">Θ</span></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-e45j"></td>
+    <td class="tg-e45j">
+      <router-link to="1">1</router-link> 
+      <router-link to="1">1</router-link>
+    </td>
+    <td class="tg-e45j">
+      <router-link to="2">2</router-link>
+    </td>
+  </tr>
+</tbody>
+</table>
 </template>
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-e45j{background-color:#6200c9;border-color:inherit;color:#ffffff;text-align:center;vertical-align:top}
+</style>
--- src/views/3.vue
+++ src/views/3.vue
@@ -1,3 +1,35 @@
 <template>
-    FILL THIS IN
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-9x9e"><span style="font-weight:var(--table-header-weight)">Ψ</span></th>
+    <th class="tg-do9z"><span style="font-weight:var(--table-header-weight)">Δ</span></th>
+    <th class="tg-do9z"><span style="font-weight:var(--table-header-weight)">Θ</span></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-do9z">
+        <router-link to="2">2</router-link>
+    </td>
+    <td class="tg-do9z">
+        <router-link to="1">1</router-link>
+    </td>
+    <td class="tg-do9z">
+        <router-link to="3">3</router-link>
+    </td>
+  </tr>
+</tbody>
+</table>
 </template>
+
+    
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-9x9e{background-color:#6200c9;border-color:inherit;color:#38fff8;text-align:left;vertical-align:middle}
+.tg .tg-do9z{background-color:#6200c9;color:#38fff8;text-align:left;vertical-align:middle}
+</style>
