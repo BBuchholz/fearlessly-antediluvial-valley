3ad929f948c6c22019de3bf8014e4698e6d13c65
Brent Buchholz <brentbuchholz@gmail.com>
2023-05-22 21:04:15 -0500

    Add MAT 5

--- src/views/5.vue
+++ src/views/5.vue
@@ -1,3 +1,175 @@
 <template>
-    FILL THIS IN
-</template>
+    <p>build a template at 
+      <a 
+        href="https://www.tablesgenerator.com/html_tables" 
+        target='_blank'
+      >
+        tablesgenerator.com
+      </a>
+    </p>
+    <p>Also, add PHLAT associations</p>
+    <table class="tg">
+      <thead>
+        <tr>
+          <th class="tg-e45j">Ψ</th>
+          <th class="tg-e45j">Δ</th>
+          <th class="tg-e45j">Σ</th>
+          <th class="tg-e45j">Θ</th>
+          <th class="tg-e45j">Φ</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="tg-e45j"></td>
+          <td class="tg-e45j">
+            <router-link to="5">5</router-link>
+            <router-link to="1">1</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="6">6</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="5">5</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="3">3</router-link>
+          </td>
+        </tr>
+        <tr>
+          <td class="tg-e45j">
+            <router-link to="4">4</router-link>
+            <router-link to="2">2</router-link>
+          </td>
+          <td class="tg-e45j"></td>
+          <td class="tg-e45j">
+            <router-link to="6">6</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="8">8</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="1">1</router-link>
+          </td>
+        </tr>
+        <tr>
+          <td class="tg-e45j"></td>
+          <td class="tg-e45j">
+            <router-link to="3">3</router-link>
+            <router-link to="3">3</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="6">6</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="9">9</router-link>
+          </td>
+          <td class="tg-e45j">
+            <router-link to="0">0</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p></p>
+    <table class="tg">
+    <thead>
+      <tr>
+        <th class="tg-e45j">Ψ</th>
+        <th class="tg-e45j">Δ</th>
+        <th class="tg-e45j">Σ</th>
+        <th class="tg-e45j">Θ</th>
+        <th class="tg-e45j">Φ</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="tg-e45j">
+          <router-link to="0">0</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="5">5</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="5">5</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="0">0</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="4">4</router-link>
+        </td>
+      </tr>
+      <tr>
+        <td class="tg-e45j">
+          <router-link to="4">4</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="1">1</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="5">5</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="4">4</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="2">2</router-link>
+        </td>
+      </tr>
+      <tr>
+        <td class="tg-e45j">
+          <router-link to="2">2</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="3">3</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="5">5</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="6">6</router-link>
+        </td>
+        <td class="tg-e45j">
+          <router-link to="0">0</router-link>
+        </td>
+      </tr>
+    </tbody>
+    </table>
+  </template>
+  <style type="text/css">
+  
+  .tg  {
+    border-collapse:collapse;
+    border-spacing:0;
+  }
+  
+  .tg td {
+    border-color:black;
+    border-style:solid;
+    border-width:1px;
+    font-family:Arial, sans-serif;
+    font-size:14px;
+    overflow:hidden;
+    padding:10px 5px;
+    word-break:normal;
+  }
+  
+  .tg th{border-color:black;
+    border-style:solid;
+    border-width:1px;
+    font-family:Arial, sans-serif;
+    font-size:14px;
+    font-weight:normal;
+    overflow:hidden;
+    padding:10px 5px;
+    word-break:normal;
+  }
+  
+  .tg .tg-e45j {
+    background-color:#6200c9;
+    border-color:inherit;
+    color:#ffffff;
+    text-align:center;
+    vertical-align:top;
+  }
+  
+  </style>
