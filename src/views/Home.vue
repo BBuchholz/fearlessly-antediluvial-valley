b0da673098dd9fae46ef0f962d861a0ce709925a
Brent Buchholz <brentbuchholz@gmail.com>
2023-05-31 21:07:10 -0500

    Add SIS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,45 @@
+<script setup>
+
+  import { ref } from 'vue'
+
+  import StepInStone from '../components/StepInStone.vue'
+
+  const stepWxrds = ref([
+    {
+      wxrdvalue: 'some value',
+      wxrdtype: 'some value',
+      content: 'some value',
+      related: 'some value',
+      uuid: '1',
+      image: 'LIBER_ABA_COVER.png'
+    },
+    {
+      wxrdvalue: 'some other value',
+      wxrdtype: 'some value',
+      content: 'some value',
+      related: 'some value',
+      uuid: '2',
+      image: 'THE_SATANIC_WITCH_COVER.png'
+    }
+  ])
+
+</script>
+
 <template>
-    <h1> Home </h1>
-  </template>
+
+  <h1> Home </h1>
+  
+  <StepInStone
+    v-for="wxrd in stepWxrds"
+    :uuid="wxrd.uuid"
+    :wxrdvalue="wxrd.wxrdvalue"
+    :wxrdtype = "wxrd.wxrdtype"
+    :content = "wxrd.content"
+    :related = "wxrd.related"
+    :image = "wxrd.image"
+  />
+
+</template>
   
   
   
